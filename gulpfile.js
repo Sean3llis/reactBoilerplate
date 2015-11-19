3bb282abaea6561c2b70c9bdc4a0fb9100ef366b
Sean Ellis <seane235@gmail.com>
2015-11-19 17:58:54 -0500

    bare up and running

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
+var WebpackDevServer = require("webpack-dev-server");
 
 gulp.task('pack', function(callback){
-    webpack(webpackConfig, function(err, stats) {
+    var compiler = webpack(webpackConfig, function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString({
             // output options
         }));
         callback();
+    });
+
+    new WebpackDevServer(compiler, {
+        // server and middleware options
+    }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        // Server listening
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+
+        // keep the server alive or continue?
+        // callback();
     });
 });
